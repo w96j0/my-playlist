caf1c8b2b55059293d09c0a852d58f3c20832a7e
ttseng <ddv8uc@hotmail.com>
2023-06-06 10:51:55 +0200

    add SpringBoot module for backend

---
+++ Backend/my-playlist-backend/src/main/java/com/valantic/myplaylist/MyPlaylistBackendApplication.java
@@ -0,0 +1,13 @@
+package com.valantic.myplaylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPlaylistBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPlaylistBackendApplication.class, args);
+    }
+
+}
---
+++ Backend/my-playlist-backend/src/test/java/com/valantic/myplaylist/MyPlaylistBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valantic.myplaylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPlaylistBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
