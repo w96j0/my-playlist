f45972a6ee9386d415298a07aecd473d4b6cfdcb
ttseng <ddv8uc@hotmail.com>
2023-06-07 16:21:14 +0200

    Change the file name

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,16 +1,12 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Playlist from './components/Playlist.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125"/>
   </header>
-
+  <Playlist msg="You did it!"/>
 </template>
 
 <style scoped>
